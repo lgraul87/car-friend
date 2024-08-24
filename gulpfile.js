d0d9bf9e4ccc72772acfa5a43450851d190d23aa
lgraul87 <lgraul87@gmail.com>
2024-08-25 01:41:27 +0200

    file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Usar 'sass' para el motor Sass
+const sass = require('gulp-sass')(require('sass')); // Usar 'sass' como el motor Sass
 
 gulp.task('styles', function() {
   return gulp.src('src/scss/**/*.scss')  // Ruta a tus archivos SCSS
