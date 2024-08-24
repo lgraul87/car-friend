356c7b2afa26e2f53b3aab12ca505e0057632863
lgraul87 <lgraul87@gmail.com>
2024-08-25 01:38:20 +0200

    css

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,10 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const concat = require('gulp-concat'); // Ejemplo para concatenar archivos JS
+const sass = require('gulp-sass')(require('sass')); // Usar 'sass' para el motor Sass
 
 gulp.task('styles', function() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src('src/scss/**/*.scss')  // Ruta a tus archivos SCSS
     .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'));  // Carpeta de destino
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')  // Ruta a tus archivos JS
-    .pipe(concat('main.js'))  // Concatenar todos los archivos JS
-    .pipe(gulp.dest('dist/js'));  // Carpeta de destino
-});
-
-gulp.task('build', gulp.series('styles', 'scripts')); // Ejecuta 'styles' y luego 'scripts'
-
-gulp.task('default', gulp.series('build'));
+gulp.task('default', gulp.series('styles'));
